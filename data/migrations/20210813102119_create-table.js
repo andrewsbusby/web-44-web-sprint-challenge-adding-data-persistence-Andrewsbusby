d02baa85089397df8946c01c34458250a3fe548b
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-13 10:44:52 -0600

    mirgated to lastest and database created

--- data/migrations/20210813102119_create-table.js
+++ data/migrations/20210813102119_create-table.js
@@ -2,26 +2,26 @@
 exports.up = function(knex) {
   return knex.schema.createTable('projects', tbl => {
     tbl.increments('project_id')
-    tbl.text('project_name', 128).notNullable()
-    tbl.text('project_description', 300)
+    tbl.string('project_name', 128).notNullable()
+    tbl.string('project_description', 300)
     tbl.boolean('project_completed')
   }),
 
   knex.schema.createTable('resources', tbl => {
     tbl.increments('resource_id')
-    tbl.text('resource_name', 128).notNullable().unique()
-    tbl.text('resource_descriptioin', 300)
+    tbl.string('resource_name', 128).notNullable().unique()
+    tbl.string('resource_descriptioin', 300)
   }),
 
   knex.schema.createTable('tasks', tbl => {
     tbl.increments('task_id')
-    tbl.text('task_description', 300).notNullable()
-    tbl.text('task_notes', 500)
+    tbl.string('task_description').notNullable()
+    tbl.string('task_notes')
     tbl.boolean('task_completed')
     tbl.integer('project_id').unsigned()
         .references('project_id')
         .inTable('projects')
-        .onDelete('RESCRICT')
+        .onDelete('RESTRICT')
         .onUpdate('RESTRICT')
   })
 };
